1ebe2e07234be8da42957dc1e29669593f691e11
Kumar Umang <umang7622@gmail.com>
2025-02-06 14:45:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// for(let i=1;i<=15;i++){
-//     if(i%2==1){
-//         console.log(i);
-//     }
-// }
-// for(let i=1;i<=15;i+=2){
-//     console.log(i);
-// }
-// for(let i=1;i<=10;i++){
-//     console.log(`5*${i} = ${5*i}`);
-// }
-//movie-guessing game
-// const favMovie="KGF";
-// let guess=prompt("Guess the movie");
-// while(guess!=favMovie&&guess!='quit'){
-//     console.log("You are wrong!!Try again");
-//     guess=prompt("Guess the movie");
-// }
-// if(guess==favMovie){
-//     console.log("You won");
-// }
-// else{
-//     console.log("Better luck next time");
-// }
 //To-do list
  let to_do=[];
  let req=prompt("Enter your request!!");
