0b98e87c0e3db4ffebfdef08cf778a27d6e1d94b
KumarUmang25 <umang7622@gmail.com>
2025-02-18 23:10:26 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let btn=document.querySelector('button');
+let h3=document.querySelector('h3');
+let div=document.querySelector('div');
+btn.addEventListener("click",function(){
+    h3.innerText=getRandomColor();
+    div.style.backgroundColor=getRandomColor();
+})
+
+
+function getRandomColor(){
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+
+    let color=`rgb(${red},${green},${blue})`;
+    return color;
+}
